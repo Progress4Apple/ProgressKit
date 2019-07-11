e632ed7a6dec239ac3c285d5cd75f7c2aed9ba6d
Marco Betschart <email@marco.betschart.name>
2019-07-11 20:20:31 +0200

    Initial commit

---
+++ ProgressKit/ProgressKit.h
@@ -0,0 +1,19 @@
+//
+//  ProgressKit.h
+//  ProgressKit
+//
+//  Created by Marco Betschart on 11.07.19.
+//  Copyright © 2019 Progress for Apple Reminders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProgressKit.
+FOUNDATION_EXPORT double ProgressKitVersionNumber;
+
+//! Project version string for ProgressKit.
+FOUNDATION_EXPORT const unsigned char ProgressKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressKit/PublicHeader.h>
+
+
